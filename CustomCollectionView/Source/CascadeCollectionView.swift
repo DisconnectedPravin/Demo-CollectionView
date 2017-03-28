1ef8bb668cf63ab9ab0208ba35bbb09bb1fdccd8
Pravin Domalwar <pravin.domalwar@accenture.com>
2017-03-28 13:59:31 +0530

    Added the Pod Specification

---
+++ CustomCollectionView/Source/CascadeCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  CascadeCollectionView.swift
+//  CustomCollectionView
+//
+//  Created by Domalwar, Pravin on 23/03/17.
+//  Copyright © 2017 Domalwar, Pravin. All rights reserved.
+//
+
+import UIKit
+
+class CascadeCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
